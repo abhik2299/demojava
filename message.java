8117d8c647f9289ecb3ab202e3b594f358b90c13
abhik2299 <abhishekgautam22999@gmail.com>
2023-06-14 10:43:09 +0530

    added

--- message.java
+++ message.java
@@ -1,2 +1,7 @@
-package com.basicjava;public class message {
+package com.basicjava;
+
+public class message {
+    public static void main(String[] args) {
+        System.out.println("We can begit  blind to the obvious and we are also blind to your blindness");
+    }
 }
